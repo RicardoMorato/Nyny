f90292c5bd89509b75c9fbd8bea19de164189d09
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-05 22:20:24 -0300

    feat(tokenizer): Added __TOKEN_TYPES__

--- src/main.py
+++ src/main.py
@@ -3,4 +3,3 @@ from tokenizer import Tokenizer
 with open('./tests/test.src') as test_file:
   code = test_file.read()
   tokens = Tokenizer(code)
-  tokens.show_code()
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,6 +1,18 @@
+from re import compile
+
+
 class Tokenizer:
+  __TOKEN_TYPES__ = [
+    ['def', r'/\bdef\b/'],
+    ['end', r'/\bend\b/'],
+    ['identifier', r'/\b[a-zA-Z]+\b/'],
+    ['integers', r'/\b[0-9]+\b/'],
+    ['o_paren', r'/\(/'],
+    ['c_paren', r'/\)/'],
+  ]
+
   def  __init__(self, code):
     self.code = code
 
-  def show_code(self):
-    print(self.code)
+  # def tokenize(self):
+  #   while not len(self.code) == 0:
